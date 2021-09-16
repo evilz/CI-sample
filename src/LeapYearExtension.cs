906df9e863cdcf170b2f585c7110970aa2eeee56
evilz <evilznet@gmail.com>
2021-09-16 18:27:34 +0200

    init

--- src/LeapYearExtension.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-public static class LeapYearExtension
-{
-    private static bool IsDivisibleBy(this int number, int divisor) => number % divisor == 0;
-    public static bool IsLeapYear(this int year) => IsDivisibleBy(year, 400) || (!IsDivisibleBy(year, 100) && IsDivisibleBy(year, 4));
-    public static bool IsLeapYear(this DateTime date) => IsLeapYear(date.Year);
-}
